96694099e1b35d7401b2917c09a1e98f2a4405d9
Kost9in <Kost9inbi4@mail.ru>
2018-01-07 16:15:40 +0200

    Update server config

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	port: 3000,
-	// add account credentialsAdd GitHub credentials to increase API requests limit or access to private repositories
+	// Add GitHub credentials to increase API requests limit or access to private repositories
 	// gitHubAccount: {
 	// 	username: 'USERNAME',
 	// 	password: 'PASSWORD'
